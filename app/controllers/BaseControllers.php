899e29d1781b79b223c606120edbf9126f04d3f1
HunterBaoc <52429760@qq.com>
2022-10-03 21:08:29 +0800

    测试twig功能

--- app/controllers/BaseControllers.php
+++ app/controllers/BaseControllers.php
@@ -2,9 +2,36 @@
     namespace controllers;
 
     class BaseControllers {
+
+        protected $twig;
+        protected $data = [];
         /**
          * 
          */
+
+        public function __construct(){
+            $loader = new \Twig\Loader\FilesystemLoader('/data/www/default/sryinges/app/views');
+            $this->twig = new \Twig\Environment($loader, [
+                //'cache' => '/path/to/compilation_cache',
+            ]);
+        }
+
+        protected function assign($var, $value=null){
+            // 如果$var是数组，则把$var中的内容追加到data中
+            if(is_array($var)){
+                $this->data = array_merge($this->data, $var);
+            // 如果$var不是数组，则把$value值赋给data中
+            }else{
+                $this->data[$var] = $value;
+            }
+        }
+
+        protected function display($template){
+            echo $this->twig->render($template.'.html', $this->data);
+        }
+
+
+
         protected function success($url, $mess){
             echo "<script>";
             echo "alert('{$mess}');";
--- app/controllers/admin/Admin.php
+++ app/controllers/admin/Admin.php
@@ -8,12 +8,14 @@
     class Admin extends BaseControllers{
         
         function index(){
-        //     $loader = new \Twig\Loader\FilesystemLoader('/path/to/templates');
-        //     $twig = new \Twig\Environment($loader, [
-        //         //'cache' => '/path/to/compilation_cache',
-        //     ]);
+            // 测试assign方法，给一个变量赋值
+            $this->assign('name','曹亚存');
+            $this->assign('title','This is a test');
+            //  数组
+            $this->assign(['aaa','bbb','ccc']);
 
-            echo "Test Macaw ";
+            $this->display('admin/index/index');
+
         }
         
     }
